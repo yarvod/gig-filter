ff70c94a1534040744c417b7fa6dcedfdc87424f
yarvod <yar.vod.ole@gmail.com>
2023-06-13 17:42:26 +0300

    modify build bat

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-pyInstaller main.py -n GIG_filter_manager --onedir
+pyInstaller main.py -n YIG_filter_manager --onedir --noconsole --icon=.\assets\logo.png -y
+copy calibration.csv .\dist\YIG_filter_manager\calibration.csv
+mkdir .\dist\YIG_filter_manager\assets
+copy .\assets\* .\dist\YIG_filter_manager\assets
