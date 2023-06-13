377711813eb8998689b6596dfa51172afebfdff7
yarvod <yar.vod.ole@gmail.com>
2023-06-13 18:22:44 +0300

    fix build bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-pyInstaller main.py -n YIG_filter_manager --onedir --noconsole --icon=.\assets\logo.png -y
+pyInstaller main.py -n YIG_filter_manager --onedir --icon=".\assets\logo.png" --noconsole --windowed -y
 copy calibration.csv .\dist\YIG_filter_manager\calibration.csv
 mkdir .\dist\YIG_filter_manager\assets
 copy .\assets\* .\dist\YIG_filter_manager\assets
