b437e1403049bd66bdbb1fd22a67d3f178bda1fa
sisteralab <ya.vodzyanovskiy@lebedev.ru>
2023-06-22 20:31:09 +0300

    change style for QDoubleSpinBox

--- interface/style/__init__.py
+++ interface/style/__init__.py
@@ -25,4 +25,53 @@ QGroupBox:title {
     left: 10px;
     color: #514f59;
 }
+QLabel {
+    color: #464d55;
+    font-weight: 600;
+}
+QLabel#heading {
+    color: #0f1925;
+    font-size: 18px;
+    margin-bottom: 10px;
+}
+QLabel#subheading {
+    color: #0f1925;
+    font-size: 12px;
+    font-weight: normal;
+    margin-bottom: 10px;
+}
+QLineEdit {
+    border-radius: 5px;
+    border: 1px solid #e5d4ed;
+    padding: 5px 15px;
+}
+QLineEdit:focus {
+    border: 1px solid #6d72c3;
+}
+QLineEdit::placeholder {
+    color: #767e89;
+}
+QDoubleSpinBox {
+    border-radius: 5px;
+    border: 1px solid #e5d4ed;
+    padding: 5px 15px;
+}
+QDoubleSpinBox:focus {
+    border: 1px solid #6d72c3;
+}
+QDoubleSpinBox::placeholder {
+    color: #767e89;
+}
+QDoubleSpinBox::up-button {
+    background: #e5d4ed;
+    border-bottom: 1px solid white;
+    image: url(./assets/up-arrow.png);
+    width: 25px;
+}
+QDoubleSpinBox::down-button {
+    background: #e5d4ed;
+    border-top: 1px solid white;
+    image: url(./assets/down-arrow.png);
+    width: 25px;
+}
 """
