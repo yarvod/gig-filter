ab8629e94bfa2fd2ee7f6ed20a8dd7df92366ee6
sisteralab <ya.vodzyanovskiy@lebedev.ru>
2023-08-01 15:49:05 +0300

    add base ni manager

--- api/ni.py
+++ api/ni.py
@@ -1,13 +1,73 @@
+import json
+import time
+
 import requests
 
+from api.rs_fsek30 import SpectrumBlock
 from state import state
 
 
-def get_resources():
-    url = f"{state.NI_PREFIX}{state.NI_IP}/resources/"
-    response = requests.get(url)
-    return response.json()
+class NiYIGManager:
+
+    def __init__(self):
+        self.url = f"{state.NI_PREFIX}{state.NI_IP}/"
+
+    def get_devices(self):
+        url = f"{self.url}/devices/"
+        response = requests.get(url)
+        return response
+
+    def start_task(self, device: str = "Dev1"):
+        url = f"{self.url}/devices/{device}/start"
+        response = requests.post(url)
+        return response
+
+    def stop_task(self, device: str = "Dev1"):
+        url = f"{self.url}/devices/{device}/stop"
+        response = requests.post(url)
+        return response
+
+    def close_task(self, device: str = "Dev1"):
+        url = f"{self.url}/devices/{device}/close"
+        response = requests.post(url)
+        return response
+
+    def write_task(self, value: int, device: str = "Dev1"):
+        url = f"{self.url}/devices/{device}/write"
+        response = requests.post(url, data=json.dumps({"value": value}))
+        return response
+
+    def device_reset(self, device: str = "Dev1"):
+        url = f"{self.url}/devices/{device}/reset"
+        response = requests.post(url)
+        return response
 
 
 if __name__ == "__main__":
-    print(get_resources())
+    url = "http://169.254.0.86/devices/Dev1/write"
+    s_block = SpectrumBlock(
+        prologix_ip=state.PROLOGIX_IP, address=state.SPECTRUM_ADDRESS
+    )
+    data = {
+        "power": [],
+        "freq": [],
+        "point": []
+    }
+    try:
+        for i in range(4096):
+            time.sleep(state.CALIBRATION_STEP_DELAY)
+            if i == 0:
+                time.sleep(0.4)
+            requests.post(url, data=json.dumps({"value": i}))
+            s_block.peak_search()
+            power = s_block.get_peak_power()
+            freq = s_block.get_peak_freq()
+            data["point"].append(i)
+            data["power"].append(power)
+            data["freq"].append(freq)
+            print(f"[STEP {i}/4095][FREQ {round(freq/1e9, 5)}]")
+    except KeyboardInterrupt:
+        pass
+
+    with open("calibration_digital.json", "w") as f:
+        json.dump(data, f)
--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,5 @@
 import os
-
-import numpy as np
+import math
 
 
 def linear(x, a, b):
@@ -8,24 +7,36 @@ def linear(x, a, b):
 
 
 def linear_fit(x, y):
-    x = np.array(x)
-    y = np.array(y)
-    # number of observations/points
-    n = np.size(x)
 
-    # mean of x and y vector
-    m_x = np.mean(x)
-    m_y = np.mean(y)
+    def mean(xs):
+        return sum(xs) / len(xs)
+    m_x = mean(x)
+    m_y = mean(y)
 
-    # calculating cross-deviation and deviation about x
-    SS_xy = np.sum(y * x) - n * m_y * m_x
-    SS_xx = np.sum(x * x) - n * m_x * m_x
+    def std(xs, m):
+        normalizer = len(xs) - 1
+        return math.sqrt(sum((pow(x1 - m, 2) for x1 in xs)) / normalizer)
 
-    # calculating regression coefficients
-    b_1 = SS_xy / SS_xx
-    b_0 = m_y - b_1 * m_x
+    def pearson_r(xs, ys):
 
-    return b_1, b_0
+        sum_xy = 0
+        sum_sq_v_x = 0
+        sum_sq_v_y = 0
+
+        for (x1, y2) in zip(xs, ys):
+            var_x = x1 - m_x
+            var_y = y2 - m_y
+            sum_xy += var_x * var_y
+            sum_sq_v_x += pow(var_x, 2)
+            sum_sq_v_y += pow(var_y, 2)
+        return sum_xy / math.sqrt(sum_sq_v_x * sum_sq_v_y)
+
+    r = pearson_r(x, y)
+
+    b = r * (std(y, m_y) / std(x, m_x))
+    a = m_y - b * m_x
+
+    return b, a
 
 
 def truncate_path(path: str):
